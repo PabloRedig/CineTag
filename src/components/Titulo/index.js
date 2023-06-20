f7a803d9b9cd9581e735882e73ba23ae4430b416
Pablo Redig <pabloleaoredig@hotmail.com>
2023-06-19 22:55:13 -0300

    Criação do card e titulo

---
+++ src/components/Titulo/index.js
@@ -0,0 +1,10 @@
+import React, { Children } from 'react'
+import styles from './Titulo.module.css'
+
+export default function Titulo ({children}) {
+  return (
+    <div className={styles.texto}>
+        {children}
+    </div>
+  )
+}
--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,6 +1,10 @@
 import Banner from "components/Banner";
+import Card from "components/Cards";
 import Rodape from "components/Rodape";
+import Titulo from "components/Titulo";
 import Cabecalho from "components/cabecalho";
+import videos from 'json/db.json'
+import styles from './Inicio.module.css'
 import React from "react";
 
 export default function Inicio() {
@@ -8,6 +12,14 @@ export default function Inicio() {
     <>
       <Cabecalho />
       <Banner imagem="home" />
+      <Titulo> 
+        <h1> Um lugar para guardar seus videos e filmes!</h1>
+      </Titulo>
+      <section className={styles.container}>
+        {videos.map((video) =>{
+          return <Card {...video} key={video.id} />
+        })}
+      </section>
       <Rodape />
     </>
   );
